bfe44748abc3ef366ab171d0abdc9920bbd2dc95
Zambz <reapz16@gmail.com>
2016-03-28 10:32:08 -0700

    vid 11

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -2,12 +2,19 @@ $(function () {
     var APPLICATION_ID = "346E4E89-4637-7BF6-FFE5-E55522AADD00",
         SECRET_KEY = "D4676A5F-121A-C551-FFA2-BB04D3BD8D00",
         VERSION = "v1";
-        
+
     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
     
-    var user = new Backendless.User();
-    user.email = "reapz16@gmail.com";
-    user.password = "password";
-    Backendless.UserService.register(user);
+    var dataStore = Backendless.Persistance.of(Posts);
+    var post = new Posts({title: "My first Blog Post", content:"My first blog post content", email:"reapz16@gmail.com"});
+    dataStore.save(post);
     
 });
+
+function Posts(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
+
