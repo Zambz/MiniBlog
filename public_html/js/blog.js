63d474fe1dd4bfc7f5267e6f45f2484d9ba62ba5
Zambz <reapz16@gmail.com>
2016-03-29 09:14:24 -0700

    vid 13

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,17 +1,21 @@
 $(function () {
     var APPLICATION_ID = "346E4E89-4637-7BF6-FFE5-E55522AADD00",
-        SECRET_KEY = "D4676A5F-121A-C551-FFA2-BB04D3BD8D00",
-        VERSION = "v1";
+            SECRET_KEY = "D4676A5F-121A-C551-FFA2-BB04D3BD8D00",
+            VERSION = "v1";
 
     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
-    
-    var dataStore = Backendless.Persistance.of(Posts);
-    var post = new Posts({title: "My first Blog Post", content:"My first blog post content", authorEmail:"reapz16@gmail.com"});
+
+    var dataStore = Backendless.Persistence.of(Posts);
+    var post = new Posts({
+        title: "My first Blog Post",
+        content: "My first blog post content",
+        authorEmail: "reapz16@gmail.com"
+    });
     dataStore.save(post);
-    
+
 });
 
-function Posts(args){
+function Posts(args) {
     args = args || {};
     this.title = args.title || "";
     this.content = args.content || "";
